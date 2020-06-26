b7688fcc369ff7eb9111030a7515696bd1c2479b
rohit <rohit0311@outlook.com>
2020-06-26 10:43:01 +0530

    Updates

---
+++ precache-manifest.9c8a2a6af61e2ce2134649bf45d18d56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8630e4b3614ff001fb3111f590bb3ef9",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "2d95e9e2105869ada113",
+    "url": "/burger-builder/static/css/main.53f37c23.chunk.css"
+  },
+  {
+    "revision": "a134dc1b1ffb9860a561",
+    "url": "/burger-builder/static/js/2.3a810b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.3a810b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d95e9e2105869ada113",
+    "url": "/burger-builder/static/js/main.39d645eb.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
