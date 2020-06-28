43d0b42702f6fddf8936da9e98a08758c0ecab57
rohit <rohit0311@outlook.com>
2020-06-28 10:36:21 +0530

    Updates

---
+++ precache-manifest.9760e7e23394693d933c66a75b493571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197adb5fa0c431288a3f6051d3f10305",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "b20a9525bc296fdad8e2",
+    "url": "/burger-builder/static/css/main.44e9ac14.chunk.css"
+  },
+  {
+    "revision": "a318182accd44ce8bcba",
+    "url": "/burger-builder/static/js/2.50b8a9ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.50b8a9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20a9525bc296fdad8e2",
+    "url": "/burger-builder/static/js/main.fd48cd1b.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burger-builder/precache-manifest.bc3a9351b901ecf7da4068b3bda496a7.js"
+  "/burger-builder/precache-manifest.9760e7e23394693d933c66a75b493571.js"
 );
 
 self.addEventListener('message', (event) => {
