82dc2bc18cf7cd670608aabfa1f5ac26b33044df
rohit <rohit0311@outlook.com>
2020-06-29 18:03:38 +0530

    burger complete

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) =>{
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey =>{
-            return (
-            <li key={igKey}>
-                <span style={{textTransform:'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}
-            </li>)
-        })
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>continue to checkout!</p>
-            <Button btnType="Danger" clicked={props.purchaseCanclled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-            
-        </Aux>
-    )
-};
+class OrderSummary extends Component{
+
+    componentDidUpdate(){
+        console.log('Did update');
+    }
+
+    render(){
+              const ingredientSummary = Object.keys(this.props.ingredients).map(
+                (igKey) => {
+                  return (
+                    <li key={igKey}>
+                      <span style={{ textTransform: "capitalize" }}>
+                        {igKey}:
+                      </span>{" "}
+                      {this.props.ingredients[igKey]}
+                    </li>
+                  );
+                }
+              ); 
+
+              return (
+                <Aux>
+                  <h3>Your Order</h3>
+                  <p>A delicious burger with following ingredients</p>
+                  <ul>{ingredientSummary}</ul>
+                  <p>
+                    <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+                  </p>
+                  <p>continue to checkout!</p>
+                  <Button btnType="Danger" clicked={this.props.purchaseCanclled}>
+                    CANCEL
+                  </Button>
+                  <Button btnType="Success" clicked={this.props.purchaseContinued}>
+                    CONTINUE
+                  </Button>
+                </Aux>
+              );
+            }
+}
+
 
 export default OrderSummary;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,15 +3,18 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const Toolbar = (props) =>(
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            <NavigationItems />
-        </nav>
-    </header>
+const Toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClick}/>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default Toolbar;
